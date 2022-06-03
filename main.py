2eef5423c2fab5d563a5b5377f132bb9e4423e97
Gizar <gizarir@gmail.com>
2022-06-03 19:46:48 +0300

    Define class Board

--- main.py
+++ main.py
@@ -66,10 +66,12 @@ class Ship:
 
     # возвращает список всех точек корабля
     def dots(self):
+        start_x = self.__dot_top.get_coord()[0]
+        start_y = self.__dot_top.get_coord()[1]
         if self.__position == 'horiz':
-            return [Dot(self.__dot_top.get_coord()[0], x) for x in range(1, self.__length+1)]
+            return [Dot(self.__dot_top.get_coord()[0], x) for x in range(start_x, start_x + self.__length)]
         else:
-            return [Dot(x, self.__dot_top.get_coord()[0]) for x in range(1, self.__length + 1)]
+            return [Dot(x, self.__dot_top.get_coord()[0]) for x in range(start_y, start_y + self.__length)]
 
     # возвращает кортеж с описанием корабля
     def get_ship(self):
@@ -88,17 +90,60 @@ class Ship:
         except TypeDotException:
             print(f'Не верный тип данных в переменной {dot_}. Требуется Dot')
 
+class Board:
+    """Базовый класс: Board - Доска для игры в морской бой"""
+    def __init__(self):
+        self.__b_status = [["0" for y in range(6)] for x in range(6)]
+        self.__ships = []  # список объектов типа Ship
+        self.__hid = True  # должно быть bool
+        self.__num_live_ship = 0  # количество живых кораблей
+
+    # добавляем корабль на доску
+    def add_ship(self, ship_):
+        # ship_ = Ship(4, Dot(1, 1), "horiz")
+        for desk in ship_.get_ship()[3]:
+            self.__b_status[desk[0] - 1][desk[1] - 1] = "■"
+
+    # обводим контур кораблю на игровой доске - получаем список точек Dot куда нельзя ставить
+    @staticmethod
+    def contour(ship_):
+        contour_ = []
+        max_x_ = None
+        max_y = None
+        for desk in ship_.get_ship()[3]:
+            max_x = desk[0] + 2 if desk[0] + 2 < 6 else 6
+            max_y = desk[1] + 2 if desk[1] + 2 < 6 else 6
+            x = desk[0]
+            y = desk[1]
+            for i in range(x - 1, max_x + 1):
+                for j in range(y - 1, max_y + 1):
+                    if Dot(i, j) not in contour_:
+                        contour_.append(Dot(i, j))
+                    else:
+                        continue
+        return contour_
+
+    # отрисовываем поле боя
+    def show(self):
+        board_show = ""
+        count = 1
+        board_show = "    " + " | ".join([str(x) for x in range(1, 6 + 1)]) + "\n"
+        for i in self.__b_status:
+            board_show += str(count) + " | " + " | ".join(i) + " \n"
+            count += 1
+        return board_show
 
 
-
-
-dot_1 = Dot(1, 2)
+dot_1 = Dot(1, 1)
 # print(dot_1.get_coord()[0])
-
-ship_1 = Ship(4, Dot(1, 1), "horiz")
-
-print(ship_1.get_ship())
-
-ship_1.set_health(dot_1)
+# проверить добавление корабля!!!
+ship_1 = Ship(1, Dot(3, 4), "horiz")
 
 print(ship_1.get_ship())
+
+board_1 = Board()
+print(board_1.show())
+
+board_1.add_ship(ship_1)
+print(board_1.show())
+print([x.get_coord() for x in board_1.contour(ship_1)])
